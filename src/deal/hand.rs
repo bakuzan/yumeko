43119c810a485d635957e3de6b19262fb3fd704b
bakuzan <imannoyed4days@hotmail.co.uk>
2018-07-09 15:56:13 +0100

    fix compile errors.

--- src/deal/hand.rs
+++ src/deal/hand.rs
@@ -18,16 +18,14 @@ impl Hand {
         self.values.to_vec()
     }
 
-    pub fn add(&self, new_card: Card) {
-        let mut values = self.values;
-        values.push(new_card);
-        self.values = values.to_vec();
+    pub fn add(&mut self, new_card: Card) {
+        self.values.push(new_card);
     }
 
     pub fn total(&self) -> u32 {
         let mut total = 0;
         // TODO handle ace being 1 or 11
-        for c in self.values {
+        for c in &self.values {
             total += c.value;
         }
 
